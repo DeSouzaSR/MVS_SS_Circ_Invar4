60bbe6c67546c6f153222d61c814af3931d8dda7
DeSouzaSR <sandro.fisica@gmail.com>
2018-08-31 18:21:36 -0300

    Finish semi-major axis analysis.

--- develop/bla.py
+++ develop/bla.py
@@ -1,13 +1,9 @@
-
 # coding: utf-8
 
-# # Semi-major axis
-# This notebook analise the time evolution of the semi-major axis for each planet. 
+# Semi-major axis
+# This program analyse the time evolution of the semi-major axis for each
+# planet. 
 
-# In[1]:
-
-
-#get_ipython().run_line_magic('matplotlib', 'inline')
 import os
 import numpy as np
 import matplotlib.pyplot as plt
@@ -15,29 +11,24 @@ import matplotlib
 import pandas as pd
 import platform
 
-
-# In[2]:
-
-
-#Matplotlib configure
+# Matplotlib configure
 plt.style.use('ggplot')
 font = {'size'   :  16}
 matplotlib.rc('font', **font)
 
 
-# In[3]:
-
-
 def create_columns(simulations):
     """Create a list of columns for dataframe"""
-    return ['time'] + ['ss_{}'.format(simulation) for simulation in simulations]
+    return ['time'] + \
+           ['ss_{}'.format(simulation) for simulation in simulations]
 
 
 def init_df(planet, simulations):
     """Initialize an empty dataframe"""
     columns = create_columns(simulations)
     df = pd.DataFrame(columns=columns)
-    df['time']= np.genfromtxt('{}0/{}.txt'.format(prefix_simulation,planet))[:,0]
+    df['time']= np.genfromtxt(
+                '{}0/{}.txt'.format(prefix_simulation,planet))[:,0]
     return df
 
 
@@ -46,20 +37,19 @@ def read_orbital_element(planet, orbital_element, simulations):
     df = init_df(planet, simulations)
     for simulation in simulations:
         os.chdir('{}{}'.format(prefix_simulation, simulation))
-        df['ss_{}'.format(simulation)] = np.genfromtxt('{}.txt'.format(planet))[:,orbital_element]
+        df['ss_{}'.format(simulation)] = np.genfromtxt('{}.txt'.format(\
+                                         planet))[:,orbital_element]
         os.chdir('..')
     return df
 
 
 def create_data_planet(planet, orbital_element, simulations):
     """Create data frame for all planets together by each orbital element"""
-    oe = {'time':0, 'a':1, 'e':2, 'inc':3, 'capom':4, 'omega':5, 'capm':6, 'peri':7, 'apo':8, 'obar':9}
-    list_df_planets = [read_orbital_element(planet, oe[orbital_element], simulations) for planet in planets]
+    oe = {'time':0, 'a':1, 'e':2, 'inc':3, 'capom':4, 'omega':5, 'capm':6, \
+          'peri':7, 'apo':8, 'obar':9}
+    list_df_planets =[read_orbital_element(planet, oe[orbital_element], simulations) for planet in planets]
     data_planets = pd.concat(list_df_planets, keys=planets)
     return data_planets
-
-
-# In[11]:
 
 
 # Paths configuration
@@ -77,73 +67,30 @@ path_ss_data = 'data/ss'
 prefix_simulation = 'ss-'
 planets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune']
 
-
-# In[13]:
-
-
 os.chdir(path_proj)
 os.chdir(path_ss_data)
 planet = 'Mercury' # for time reference
 orbital_element = 'a'
-n_lines = 96
+n_lines = 96 # Number of simulations
 simulations = np.arange(n_lines)
-
-
-# In[14]:
-
 
 data_planet = create_data_planet('Mercury', 'a', simulations)
 
-
-# In[15]:
-
-
-data_planet
-
-
-# In[17]:
-
-
+time = data_planet.loc['Mercury']['time']
 mercury = data_planet.loc['Mercury']
 
-
-# In[18]:
-
-
-mercury
-
-
-# ## Evolution of the Mercury's semi-major axis
-
-# In[28]:
-
-
+# Plot Mercury's semi-major axis
 mercury.set_index('time').plot.line(legend=False, figsize=(14,10))
+plt.show()
 
-
-# In[33]:
-
-
-time = data_planet.loc['Mercury']['time']
-venus = data_planet.loc['Venus']
-earth = data_planet.loc['Earth']
-mars = data_planet.loc['Mars']
-
-
-# In[44]:
-
-
+# Plot Inner's semi-major axis
 ax = plt.gca()
-inners = planets[0:4]
-for planet in inners:
+for planet in  planets[0:4]:
     data_planet.loc[planet].set_index('time').plot(ax = ax, legend=False, figsize=(14,10))
+plt.show()
 
-
-# In[47]:
-
-
+# Plot giant's semi-major axis
 ax = plt.gca()
-giants = planets[4:8]
-for planet in giants:
+for planet in planets[4:8]:
     data_planet.loc[planet].set_index('time').plot(ax = ax, legend=False, figsize=(14,10))
-
+plt.show()
